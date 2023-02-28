d492cd1932af24677351e6c9abf18c8b6ab92eeb
liav2002 <liavmail00@gmail.com>
2023-02-28 20:02:12 +0200

    initailize projects files.

---
+++ TEE_8442_0871Host/TEE_8442_0871Host/Program.cs
@@ -0,0 +1,68 @@
+﻿using Intel.Dal;
+using System;
+using System.Text;
+using System.IO;
+
+namespace TEE_8442_0871Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if AMULET
+            // When compiled for Amulet the Jhi.DisableDllValidation flag is set to true 
+			// in order to load the JHI.dll without DLL verification.
+            // This is done because the JHI.dll is not in the regular JHI installation folder, 
+			// and therefore will not be found by the JhiSharp.dll.
+            // After disabling the .dll validation, the JHI.dll will be loaded using the Windows search path
+			// and not by the JhiSharp.dll (see http://msdn.microsoft.com/en-us/library/7d83bc18(v=vs.100).aspx for 
+			// details on the search path that is used by Windows to locate a DLL) 
+            // In this case the JHI.dll will be loaded from the $(OutDir) folder (bin\Amulet by default),
+			// which is the directory where the executable module for the current process is located.
+            // The JHI.dll was placed in the bin\Amulet folder during project build.
+            Jhi.DisableDllValidation = true;
+#endif
+
+            Jhi jhi = Jhi.Instance;
+            JhiSession session;
+
+            // This is the UUID of this Trusted Application (TA).
+            //The UUID is the same value as the applet.id field in the Intel(R) DAL Trusted Application manifest.
+            string appletID = "d29f1599-d31a-4d31-9a63-01b67e5b5a48";
+            // This is the path to the Intel Intel(R) DAL Trusted Application .dalp file that was created by the Intel(R) DAL Eclipse plug-in.
+            string project_directory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent.FullName;
+            string dalpPath = "\\TEE_8442_0871\\bin\\TEE_8442_0871.dalp";
+            Console.Write("$appletPath = " + project_directory + dalpPath + "\n\n");
+            string appletPath = project_directory + dalpPath;
+
+            // Install the Trusted Application
+            Console.WriteLine("Installing the applet.");
+            jhi.Install(appletID, appletPath);
+
+            // Start a session with the Trusted Application
+            byte[] initBuffer = new byte[] { }; // Data to send to the applet onInit function
+            Console.WriteLine("Opening a session.");
+            jhi.CreateSession(appletID, JHI_SESSION_FLAGS.None, initBuffer, out session);
+
+            // Send and Receive data to/from the Trusted Application
+            byte[] sendBuff = UTF32Encoding.UTF8.GetBytes("Hello"); // A message to send to the TA
+            byte[] recvBuff = new byte[2000]; // A buffer to hold the output data from the TA
+            int responseCode; // The return value that the TA provides using the IntelApplet.setResponseCode method
+            int cmdId = 1; // The ID of the command to be performed by the TA
+            Console.WriteLine("Performing send and receive operation.");
+            jhi.SendAndRecv2(session, cmdId, sendBuff, ref recvBuff, out responseCode);
+            Console.Out.WriteLine("Response buffer is " + UTF32Encoding.UTF8.GetString(recvBuff));
+
+            // Close the session
+            Console.WriteLine("Closing the session.");
+            jhi.CloseSession(session);
+
+            //Uninstall the Trusted Application
+            Console.WriteLine("Uninstalling the applet.");
+            jhi.Uninstall(appletID);
+
+            Console.WriteLine("Press Enter to finish.");
+            Console.Read();
+        }
+    }
+}
