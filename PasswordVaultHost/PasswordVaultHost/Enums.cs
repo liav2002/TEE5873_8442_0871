85380c65a272f0b27c191253fcc62953fa4d85a8
liav2002 <liavmail00@gmail.com>
2023-07-24 15:39:38 +0300

    add enums

---
+++ PasswordVaultHost/PasswordVaultHost/Enums.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PasswordVaultHost
+{
+    enum AppletResult
+    { 
+        RES_FAILED = 0,
+        RES_SUCCESS = 1,
+        RES_NOT_SIGNED_IN = 2,
+        RES_NOT_REGISTERED = 3,
+        RES_WRONG_PASSWORD = 4,
+        RES_NEW_PSWD = 5,
+        RES_USERNAME_MISSING = 6
+    }
+
+    enum AppletOperation
+    {
+        RESET_MEMORY = 0,
+        GET_PASSWORD = 1,
+        GET_USERNAME = 2,
+        SIGN_IN = 3,
+        REGISTER = 4
+    }
+
+}
